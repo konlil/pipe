b1149288c8791f5795544542ace3183495c422f3
konlil <jg.yuan@gmail.com>
2011-12-26 18:02:23 +0800

    FIX: camera bug

---
+++ Pipe/Helper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Diagnostics;
+
+namespace Pipe
+{
+    public class Helper
+    {
+        public static void Log(string format, params Object [] objlist)
+        {
+            String str = String.Format(format, objlist);
+            Debug.WriteLine(str);
+        }
+    }
+}
